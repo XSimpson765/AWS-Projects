a5f9be99e3fa6c7e4bdcaeb41a29ecb023630b16
XSimpson765 <simpson.xavier2423@gmail.com>
2026-03-02 23:43:12 +0000

    tf

--- project-01-secure-web-baseline/terraform/versions.tf
+++ project-01-secure-web-baseline/terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "terraform-20260302203714845100000001"
+    bucket         = "xsimpson-tf-state-668429212775"
     key            = "project-01/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
---
+++ project-02-terraform-backend/terraform.tfvars
@@ -0,0 +1,1 @@
+state_bucket_name = "xsimpson-tf-state-668429212775"
