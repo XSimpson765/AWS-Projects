5087adbd59a22466f50768178415288fe4243671
XSimpson765 <simpson.xavier2423@gmail.com>
2026-03-03 02:59:11 +0000

    rd

---
+++ project-03-prod-web-platform/terraform/scaling.tf
@@ -0,0 +1,13 @@
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "${local.name_prefix}-cpu-tt"
+  autoscaling_group_name = aws_autoscaling_group.this.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0
+  }
+}
