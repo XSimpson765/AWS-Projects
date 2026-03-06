543a4a924c92212a2755caa32192915a1b49f82b
XSimpson765 <simpson.xavier2423@gmail.com>
2026-03-06 21:46:26 +0000

    P5

---
+++ project-5-cloud-monitoring/monitoring/cloudwatch.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
+  alarm_name          = "high-cpu-utilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 70
+
+  dimensions = {
+    InstanceId = aws_instance.monitoring_instance.id
+  }
+
+  alarm_description = "Alarm when CPU exceeds 70%"
+}
